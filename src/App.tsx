4c3211dd32472a4690667f431e2d0daee1892c6c
espisepi <josdomesp@gmail.com>
2023-01-14 03:13:06 +0100

    helloworld app1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import App1 from './espinaco/apps/app1/App1';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({ nameApp = "app1" }) {
+  if(nameApp === "app1") {
+    return (
+      <div className="App">
+        <App1 />
+      </div>
+    );
+  } else {
+    return (<h1>APP NO ENCONTRADA</h1>);
+  }
+  
 }
 
 export default App;
---
+++ src/espinaco/apps/app1/App1.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function App1() {
+    return (
+        <div className="App">
+            <h1>HELLOWORLD APP 1</h1>
+        </div>
+    );
+}
