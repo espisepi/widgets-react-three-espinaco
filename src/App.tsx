a8f148ec37b6310e75c7e3fcea53f933cd254f49
espisepi <josdomesp@gmail.com>
2023-01-14 23:24:43 +0100

    Microfrontend Widget with parcel added! :)

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import App1 from './espinaco/apps/app1/App1';
 
-function App({ nameApp = "app1" }) {
-  if(nameApp === "app1") {
+function App({ name = "app1", configuration = {} }) {
+  if(name === "app1") {
     return (
       <div className="App">
         <App1 />
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+// Find all widget divs
+const widgetDivs = document.querySelectorAll('.ultimate-react-three-espinaco, #ultimate-react-three-espinaco');
+
+// Inject our React App into each class
+widgetDivs.forEach(div => {
+  const root = ReactDOM.createRoot(div as HTMLElement);
+  const url = div.getAttribute("data-symbol") as string;
+  console.log(url);
+  root.render(
+    <React.StrictMode>
+      <App name="app1" configuration={{url:url}}/>
+    </React.StrictMode>
+  );
+});
+
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
